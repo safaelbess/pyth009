bed732786f3a9158c4cfc6663bed214acc561311
safaelbess <136888825+safaelbess@users.noreply.github.com>
2023-07-21 11:54:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,66 @@
-#  c= 1
-#  while (c<=3):
-#   num1 =int(input('enter a number'))
-#   for i in range(2,num1):
-#     if num1%i==0:
-#       print('the number is not prime')
-#       break
+# #  c= 1
+# #  while (c<=3):
+# #   num1 =int(input('enter a number'))
+# #   for i in range(2,num1):
+# #     if num1%i==0:
+# #       print('the number is not prime')
+# #       break
    
-#     else: 
-#       print('the number is prime')
+# #     else: 
+# #       print('the number is prime')
       
       
-#   if num1%2==0:
-#     print ("the number is even")
-#   else:
-#     print('the number is odd')
+# #   if num1%2==0:
+# #     print ("the number is even")
+# #   else:
+# #     print('the number is odd')
   
-#   str1=input('enter your word')  
-#   for j in range (0,len(str1)):
-#     if str1[j]=='u' or str1[j]=='l':
-#       str1=str1.replce(str1[j],'*')
-#       print(str1)
+# #   str1=input('enter your word')  
+# #   for j in range (0,len(str1)):
+# #     if str1[j]=='u' or str1[j]=='l':
+# #       str1=str1.replce(str1[j],'*')
+# #       print(str1)
       
-#   c =c+1  
+# #   c =c+1  
   
-number1 = int(input('enter a number')
-def ifprime(number1):
-  for i in range(2,number1):
-    ifnumber1%i==0:
-      print ('the number is not prime')
-      break
-    else:
-      print('the number is not prime')
+# number1 = int(input('enter a number')
+# def ifprime(number1):
+#   for i in range(2,number1):
+#     ifnumber1%i==0:
+#       print ('the number is not prime')
+#       break
+#     else:
+#       print('the number is not prime')
       
-    i=i+1
-  def oddoreven(number2):
-    ifnumber2%2):
-      print('even')
-    else:
-      print('odd')
+#     i=i+1
+#   def oddoreven(number2):
+#     ifnumber2%2):
+#       print('even')
+#     else:
+#       print('odd')
       
-  var1=input('enter a string')
-  def str1(var):
-    for i in (0,len(var)):
-      if var1[i]=='u'or var1[i]=='l':
-        var1=var1.replce(var1[i],'*')
-    print var1
+#   var1=input('enter a string')
+#   def str1(var):
+#     for i in (0,len(var)):
+#       if var1[i]=='u'or var1[i]=='l':
+#         var1=var1.replce(var1[i],'*')
+#     print var1
     
-  ifprime(number1)
-  oddoreven(number1)
-  str1(var1)
+#   ifprime(number1)
+#   oddoreven(number1)
+#   str1(var1)
+
+
+
+names =['David', 'Alice' , 'Caroline']
+counter =0
+
+myNames =['Ali']
+names = names[0:] + myNames[0:]
+  
+
+for i in (names):
+  if names[0] =='David':
+    names = names[1:]
+    print(names)
+
